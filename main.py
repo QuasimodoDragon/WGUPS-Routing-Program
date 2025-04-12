1c450d6ee9c69dd9ddfc21c604adb8b34b707a35
Taylor <ktaylorbailey@gmail.com>
2025-04-12 15:07:59 -0400

    Create distance betwixt function.

--- main.py
+++ main.py
@@ -50,11 +50,14 @@ except PermissionError:
 
 # ------------- Distance Matrix ------------- 
 
+# Create a distance matrix that will become a 2D array
 distance_matrix = []
 
+# Open the distances csv file and use it to create the distance matrix
 with open("data/distances.csv", "r") as csv_file:
     reader = csv.reader(csv_file, delimiter=",")
 
+    # Add every row to the distance matrix to create a 2D array
     for row in reader:
         distance_matrix.append(row)
 
@@ -63,13 +66,37 @@ with open("data/distances.csv", "r") as csv_file:
 
 # ------------- Address List -------------
 
+# Create an array to hold the addresses
 address_list = []
 
+# Open the addresses csv file and use it to create the address list
 with open("data/addresses.csv", "r") as csv_file:
     reader = csv.reader(csv_file, delimiter=",")
+    # Skip the header row
     next(reader)
 
+    # For every row add only the address to the array
     for row in reader:
         address_list.append(row[1])
 
 # print(address_list)
+
+# ------------- Distance Betwixt Function -------------
+
+# Function that returns the distance between two addresses
+def betwixt(address_1, address_2):
+    # Gets the index of both addresses from the address list
+    index_1 = address_list.index(address_1)
+    index_2 = address_list.index(address_2)
+    # Uses the indexes to find the distance between the addresses from the distance matrix
+    distance = distance_matrix[index_1][index_2]
+
+    # The distance matrix is only half filled so if statement returns if the distance is not empty
+    if distance != "":
+        return distance
+    # If the distance is empty then the indexes are swapped and the distance is returned
+    else:
+        distance = distance_matrix[index_2][index_1]
+        return distance
+
+# print(betwixt("300 State St", "1330 2100 S"))
